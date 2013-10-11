6a7b30d31aee0b03bca8ff2ea0b0a74cec10d3e5
Urs Wolfer <uwolfer@fwo.ch>
2013-10-11 21:22:35 +0200

    minor cleanups

--- src/test/java/com/urswolfer/intellij/plugin/gerrit/rest/GerritUtilTest.java
+++ src/test/java/com/urswolfer/intellij/plugin/gerrit/rest/GerritUtilTest.java
@@ -1,18 +1,30 @@
+/*
+ * Copyright 2013 Urs Wolfer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.urswolfer.intellij.plugin.gerrit.rest;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
 
 /**
- * @author <a href=mailto:uwolfer@tocco.ch>Urs Wolfer</a>
- * @since 5/26/13
+ * @author Urs Wolfer
  */
 public class GerritUtilTest {
-    @Test
-    public void testGetAvailableRepos() throws Exception {
-    }
 
     @Test
     public void testProjectNames() throws Exception {

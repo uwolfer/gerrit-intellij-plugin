095e6db09ffb989c7074035f4beb9e9549e91d68
Urs Wolfer <uwolfer@fwo.ch>
2013-10-12 13:36:23 +0200

    add action to open selected change in Gerrit

---
+++ src/main/java/com/urswolfer/intellij/plugin/gerrit/ui/action/OpenInBrowserAction.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013 Urs Wolfer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.urswolfer.intellij.plugin.gerrit.ui.action;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.util.IconLoader;
+import com.urswolfer.intellij.plugin.gerrit.rest.GerritApiUtil;
+import com.urswolfer.intellij.plugin.gerrit.rest.bean.ChangeInfo;
+
+/**
+ * @author Urs Wolfer
+ */
+public class OpenInBrowserAction extends AbstractChangeAction {
+
+    public OpenInBrowserAction() {
+        super("Open in Gerrit", "Open corresponding link in browser", IconLoader.getIcon("/icons/gerrit.png"));
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        final ChangeInfo selectedChange = getSelectedChange(anActionEvent);
+        String urlToOpen = getUrl(selectedChange);
+        BrowserUtil.launchBrowser(urlToOpen);
+    }
+
+    private String getUrl(ChangeInfo change) {
+        String url = GerritApiUtil.getApiUrl();
+        String changeNumber = change.getNumber();
+        return String.format("%s/%s", url, changeNumber);
+    }
+}

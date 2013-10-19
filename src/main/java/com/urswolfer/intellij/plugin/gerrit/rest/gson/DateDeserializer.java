a76878f913d82d070b93d17a33deacbde41c7fd5
Urs Wolfer <uwolfer@fwo.ch>
2013-10-19 14:03:04 +0200

    minor cleanups

--- src/main/java/com/urswolfer/intellij/plugin/gerrit/rest/gson/DateDeserializer.java
+++ src/main/java/com/urswolfer/intellij/plugin/gerrit/rest/gson/DateDeserializer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Urs Wolfer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.urswolfer.intellij.plugin.gerrit.rest.gson;
 
 import com.google.gson.JsonDeserializationContext;
@@ -20,7 +36,7 @@ import java.util.TimeZone;
  */
 public class DateDeserializer implements JsonDeserializer<Date> {
 
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     static {
         DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
     }

4c5aa10c7f8b497137c00598101278692188225a
Urs Wolfer <uwolfer@fwo.ch>
2025-12-23 22:00:23 +0100

    Revert "Add force branch name settings"

--- src/main/java/com/urswolfer/intellij/plugin/gerrit/push/GerritPushOptionsPanel.java
+++ src/main/java/com/urswolfer/intellij/plugin/gerrit/push/GerritPushOptionsPanel.java
@@ -34,8 +34,8 @@ public class GerritPushOptionsPanel extends VcsPushOptionsPanel {
     private final GerritPushExtensionPanel gerritPushExtensionPanel;
     private GitPushOptionsPanel gitPushOptionsPanel;
 
-    public GerritPushOptionsPanel(boolean pushToGerrit, boolean forceDefaultBranch) {
-        gerritPushExtensionPanel = new GerritPushExtensionPanel(pushToGerrit, forceDefaultBranch);
+    public GerritPushOptionsPanel(boolean pushToGerrit) {
+        gerritPushExtensionPanel = new GerritPushExtensionPanel(pushToGerrit);
     }
 
     @SuppressWarnings("UnusedDeclaration") // javassist call
--- src/main/java/com/urswolfer/intellij/plugin/gerrit/ui/SettingsPanel.java
+++ src/main/java/com/urswolfer/intellij/plugin/gerrit/ui/SettingsPanel.java
@@ -63,7 +63,6 @@ public class SettingsPanel {
     private JCheckBox showTopicColumnCheckBox;
     private JComboBox showProjectColumnComboBox;
     private JTextField cloneBaseUrlTextField;
-    private JCheckBox forceDefaultBranchCheckBox;
 
     private boolean passwordModified;
 
@@ -280,14 +279,6 @@ public class SettingsPanel {
 
     public String getCloneBaseUrl() {
         return cloneBaseUrlTextField.getText().trim();
-    }
-
-    public void setForceDefaultBranch(final boolean forceDefaultBranch) {
-        forceDefaultBranchCheckBox.setSelected(forceDefaultBranch);
-    }
-
-    public boolean getForceDefaultBranch() {
-        return forceDefaultBranchCheckBox.isSelected();
     }
 
 }
